4f7d1503e1c674401d9baac1fe482c207b21ee02
iamabhaydawar <iamabhaydawar@gmail.com>
2025-05-22 19:50:48 -0700

    2.3.2 blog downloading admin dashboard button

--- backend/celery/tasks.py
+++ backend/celery/tasks.py
@@ -8,13 +8,16 @@ def add(x,y):
     time.sleep()
     return x+y
 
-@shared_task(ignore_results = False)
-def create_csv():
+@shared_task(bind=True,ignore_results = False)
+def create_csv(self):
     resource = Blog.query.all()
+    #for creating new filenames for blog
+    task_id = self.request.id
+    filename =f'blog_data_{task_id}.csv'
     column_names =[column.name for column in Blog.__table__.columns]
     csv_out= flask_excel.make_response_from_query_sets(resource,column_names=column_names,file_type='csv')
 
     
-    with open('./backend/celery/user-downloads/blog.csv','wb') as file:
+    with open(f'./backend/celery/user-downloads/{filename}','wb') as file:
         file.write(csv_out.data)
-    return 'blog.csv'
+    return filename
