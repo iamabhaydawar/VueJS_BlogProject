91c6c17adcc664c949f2ca212ec4600bdb376cdb
iamabhaydawar <iamabhaydawar@gmail.com>
2025-05-26 13:57:13 -0700

    2.5.1 crontab email reminder

--- backend/celery/celery_schedule.py
+++ backend/celery/celery_schedule.py
@@ -7,8 +7,9 @@ celery_app=app.extensions['celery']
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
     #s-schedule task
-    sender.add_periodic_task(10.0,test.s('hello celery'))
-    sender.add_periodic_task(10.0,email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'))
+    # sender.add_periodic_task(10.0,test.s('hello celery'))
+    # sender.add_periodic_task(10.0,email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'))
+    sender.add_periodic_task(crontab(hour=13,minute=56),email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'))
 
 @celery_app.task
 def test(arg):
