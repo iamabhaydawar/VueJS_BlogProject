4bc581ab8f4b0a43aac115a3b2b04f2f3a54a9b1
iamabhaydawar <iamabhaydawar@gmail.com>
2025-05-26 14:15:54 -0700

    2.5.1 +added weekly and monthly reminders

--- backend/celery/celery_schedule.py
+++ backend/celery/celery_schedule.py
@@ -6,11 +6,16 @@ celery_app=app.extensions['celery']
 
 @celery_app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
-    #s-schedule task
-    # sender.add_periodic_task(10.0,test.s('hello celery'))
-    # sender.add_periodic_task(10.0,email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'))
-    sender.add_periodic_task(crontab(hour=13,minute=56),email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'))
+    # every 10 seconds
+    # sender.add_periodic_task(10.0, email_reminder.s('students@gmail', 'reminder to login', '<h1> hello everyone </h1>') )
 
-@celery_app.task
-def test(arg):
-    print(arg) 
+    # daily message at 2:11 pm, everyday
+    sender.add_periodic_task(crontab(hour=14, minute=11), email_reminder.s('students@gmail', 'reminder to login', '<h1> hello everyone </h1>'), name='daily reminder' )
+
+    # weekly messages
+    sender.add_periodic_task(crontab(hour=14, minute=11, day_of_week='monday'), email_reminder.s('students@gmail', 'reminder to login', '<h1> hello everyone </h1>'), name = 'weekly reminder' )
+    #monthly reminder
+    sender.add_periodic_task(crontab(hour=14, minute=11, day_of_month='26'), email_reminder.s('students@gmail', 'reminder to login', '<h1> hello everyone </h1>'), name = 'weekly reminder' )
+
+
+#s-schedule task #every 10 seconds # sender.add_periodic_task(10.0,test.s('hello celery')) # sender.add_periodic_task(10.0,email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>')) #if functional parameters same in email reminders then name should be given to that particular object #daily message at 13:56pm everyday sender.add_periodic_task(crontab(hour=14,minute=8),email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>',name='daily reminder')) #weekly reminder sender.add_periodic_task(crontab(hour=14,minute=8,day_of_week='monday'),email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'),name='weekly reminder') #monthly reminder sender.add_periodic_task(crontab(hour=14,minute=8,day_of_month='26'),email_reminder.s('students@gmail','reminder to login','<h1>hello everyone</h1>'),name='monthly reminder')
